5c6de76afea0af0e6408fbc16d1ee982f459e552
an1starkc0de <160142491+an1starkc0de@users.noreply.github.com>
2024-12-17 22:55:52 +0530

    Add files via upload

---
+++ Static/1stj.js
@@ -0,0 +1,19 @@
+// $(function(){
+//     SyntaxHighlighter.all();
+//   });
+//   $(window).load(function(){
+//     $('.flexslider').flexslider({
+//       animation:"slide",
+//       start: function(slider){
+//         $('body').removeClass('loading');
+//       }
+//     });
+//   });
+
+// const slides = document.querySelectorAll(".flexslider")
+// var counter =0 ;
+// slides.forEach(
+//     (slides, index)=>{
+//         slides.computedStyleMap.left= `${index *100}%`
+//     }
+// )
